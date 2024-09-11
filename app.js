dc23ab15cd55fb311706882a659d396ddb6ebe8b
fardinariful <fardinewucse2020@gmail.com>
2024-09-11 15:07:51 +0600

    ticket info added

--- app.js
+++ app.js
@@ -4,11 +4,33 @@
 const all_btn=document.getElementsByClassName("allbtn");
 let count=0;
 let seatcapticity=40;
+const ticketClass="Economy";
+const seatPrice=550;
 for(const btn of all_btn){
     //seat_left
     btn.addEventListener("click",function(e){
+        const seatname=e.target.innerText;
         count=count+1;
         const seatleft=seatcapticity-count;
+        
+        const ticket_info=document.getElementById("ticket_info");
+        const div=document.createElement("div");
+        div.classList.add("flex","justify-evenly","w-full")
+        const p3=document.createElement("p");
+        p3.innerText=seatname;
+        const p1=document.createElement("p");
+        p1.innerText=ticketClass;
+        const p2=document.createElement("p");
+        p2.innerText=seatPrice;
+        
+        div.appendChild(p3);
+        div.appendChild(p1);
+        div.appendChild(p2);
+       
+
+        ticket_info.appendChild(div);
+
+        
 
 
 
