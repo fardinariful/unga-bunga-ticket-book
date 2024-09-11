be41974b86f4e8f7571695720f512e874a06fcb0
fardinariful <fardinewucse2020@gmail.com>
2024-09-11 11:48:57 +0600

    js function added

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-function addbackgroundcolor(elementID){
-    const element=document.getElementById(elementID);
-    console.log(element);
+
+//console.log("boss");
+
+const all_btn=document.getElementsByClassName("allbtn");
+let count=0;
+let seatcapticity=40;
+for(const btn of all_btn){
+    //seat_left
+    btn.addEventListener("click",function(e){
+        count=count+1;
+        const seatleft=seatcapticity-count;
+
+
+
+
+
+        setInnerText("booked_seat",count);
+        setInnerText("seat_left",seatleft);
+
+})
+
 }
 
 
+    
+
+
+function setInnerText(id,value){
+    document.getElementById(id).innerText=value;
+}
